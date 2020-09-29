ccd238c8f712cc34d6e83e998fccd145db9edbe9
Bhuswarna Kashyap <bhuswarnakashyap@gmail.com>
2020-09-29 13:32:56 +0530

    Minor modifications in pharmacy analytics done

--- pharmacy_analytics/src/App.js
+++ pharmacy_analytics/src/App.js
@@ -69,9 +69,9 @@ function App() {
 
 
         <div id="content">
-          <nav className="navbar navbar-expand navbar-light">
+        <nav className="navbar navbar-expand navbar-light">
                 <div className="container-fluid">
-                  <div className="row">
+
                   <button type="button" id="sidebarCollapse" class="btn mt-n2">
                           <svg aria-hidden="true" focusable="false" height="25px" width="25px" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>      
                   </button>
@@ -91,7 +91,6 @@ function App() {
                         </li>
                     </ul>
                   </div>
-                </div>
                 </div>
           </nav>
             
